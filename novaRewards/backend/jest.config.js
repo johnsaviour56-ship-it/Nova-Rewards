e5b70ff6a9048bbbf4d767a84f964b1a9345ca1f
Ikeh Celestine <chukwudi.ikeh.232330@unn.edu.ng>
2026-03-23 22:31:33 +0100

    config: add jest.setup.js to silence console.error during tests

--- novaRewards/backend/jest.config.js
+++ novaRewards/backend/jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
   testEnvironment: 'node',
   testMatch: ['**/*.test.js'],
   verbose: true,
+  setupFilesAfterEnv: ['./jest.setup.js'],
   coverageThreshold: {
     global: {
       lines: 80,
